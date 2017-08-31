4d1e760c055785071610987289294ab83c796a48
Elmira_Zhernova <Elmira_Zhernova@epam.com>
2017-08-31 19:36:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Levian</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<h1 class='elegantshadow'>Levian</h1>
+<section>
+	<div class="main">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div class="background-img" style="background-image: url(img/1_mom.jpg)" ></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+				</div>
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div style="background-image: url(img/2_rotos.jpg)" class="background-img"></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+				</div>
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div style="background-image: url(img/3_skinny.jpg)" class="background-img"></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div class="background-img" style="background-image: url(img/4_jegging.jpg)" ></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+					<!-- <img src="img/4_jegging.jpg" class="img-responsive" alt="Responsive image"> -->
+				</div>
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div class="background-img" style="background-image: url(img/6_pushup.jpg)" ></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+					<!-- <img src="img/6_pushup.jpg" class="img-responsive" alt="Responsive image"> -->
+				</div>
+				<div class="col-xs-12 col-md-4 no-margin">
+					<div class="background-img" style="background-image: url(img/5_high_waist.jpg)" ></div>
+					<div class="pumpom">ПЛАТЬЕ</div>
+					<!-- <img src="img/5_high_waist.jpg" class="img-responsive" alt="Responsive image"> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
