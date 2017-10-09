ec050bb44c1ca3cc0eb3a1fd0dbdfa271e5a2143
Elmira Zhernova <Elmira_Zhernova@epam.com>
2017-10-09 19:39:17 +0300

    emma laptop commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<h1 class='elegantshadow'>Levian1</h1>
+<h1 class='elegantshadow'>Левиан</h1>
 <section>
 	<div class="main">
 		<div class="container-fluid">
@@ -41,6 +41,19 @@
 					<div class="background-img" style="background-image: url(img/5_high_waist.jpg)" ></div>
 					<div class="pumpom">ПЛАТЬЕ</div>
 					<!-- <img src="img/5_high_waist.jpg" class="img-responsive" alt="Responsive image"> -->
+				</div>
+			</div>
+			<h1 class='elegantshadow'>верхняя одежда</h1>
+			<div class="row">
+				<div class="col-xs-12 col-md-6 no-margin">
+					<div class="background-img" style="background-image: url(img/IMG-726eb753200f16e89a2ff116bd97e470-V.jpg)" ></div>
+					<div class="pumpom">норка милена</div>
+					<!-- <img src="img/4_jegging.jpg" class="img-responsive" alt="Responsive image"> -->
+				</div>
+				<div class="col-xs-12 col-md-6 no-margin">
+					<div class="background-img" style="background-image: url(img/IMG-b133cdbf626f32686183024d3cf3cd64-V.jpg)" ></div>
+					<div class="pumpom">норка милена</div>
+					<!-- <img src="img/6_pushup.jpg" class="img-responsive" alt="Responsive image"> -->
 				</div>
 			</div>
 		</div>
